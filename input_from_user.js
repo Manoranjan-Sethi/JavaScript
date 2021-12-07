83d9e384b7a3ee7750bf91ba046eee4147c7e79e
ManoranjanSethi <manoranjansethi97@gmail.com>
2021-12-07 13:24:44 +0530

    first commit

---
+++ input_from_user.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+var fname = prompt("Enter first name: ");
+var lname = prompt("Enter last name: ");
+
+console.log("Your full name is",fname,lname);
---
+++ intro and variables.js
@@ -0,0 +1,9 @@
+//  This command prints output
+console.log("Hello World!");
+
+//variables
+// varibles can store information
+
+var firstName = "Manoranjan"; // string
+var lastName = "Sethi";
+console.log(firstName,lastName);
