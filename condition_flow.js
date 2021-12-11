f98c2da4977ee224bd9667d96e9868b964d5fd6c
ManoranjanSethi <manoranjansethi97@gmail.com>
2021-12-11 23:49:25 +0530

    sixth

--- condition_flow.js
+++ condition_flow.js
@@ -56,3 +56,51 @@ for(var i =1;i<=num;i++){
     console.log(i);
   }
 }
+
+console.log("-----------");
+//
+var num1 = 4;
+var num2 = 8;
+var num3 = 5;
+if(num1>num2){
+  console.log(true);
+}
+else{
+  console.log(false);
+}
+num1 = num1+num3;
+if(num1>num2){
+  console.log(true);
+}
+else{
+  console.log(false);
+}
+
+console.log("==========")
+
+//
+var num1 = 4;
+var num2 = 16;
+if(num1>num2){
+  console.log(true);
+}
+else{
+  console.log(false);
+}
+if(num1<num2){
+  console.log(true);
+}
+else{
+  console.log(false);
+}
+if(num1==num2){
+  console.log(true);
+}
+else{
+  console.log(false);
+}
+
+console.log("--------");
+
+// 
+
---
+++ string_indeximg.js
@@ -0,0 +1,19 @@
+//string indexing
+//spaces are also considered as characters
+let firstName = "manoranjan";
+console.log(firstName[4]);      //index starts from 0
+
+console.log(firstName.length);  //length starts from 1
+
+
+//so last index = length - 1
+
+console.log(firstName[firstName.length - 1]); //last element of the string
+
+console.log("----------");
+
+//String Methods
+
+ let Name = "    Amit  ";
+ console.log(Name.length);
+ console.log()
