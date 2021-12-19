efc36efc45b4ca0ed007c4662d01a5172ae2f808
Manoranjan <manoranjansethi97@gmail.com>
2021-12-20 01:34:10 +0530

    string_problems

---
+++ Strings/Problems.js
@@ -0,0 +1,10 @@
+
+
+//occurence of a character in a string
+var a = "He's my king from this day until his last day";
+// var a = "qwerta";
+var count = 0;
+for (var i = a.indexOf("a"); i!==-1; i=a.indexOf("a",i + 1)){
+      count++;
+}
+console.log(count);
