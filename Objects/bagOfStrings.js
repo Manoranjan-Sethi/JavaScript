e374e5e16f944c0af66a7a0cd238e3255e96eccd
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-08-24 11:31:56 +0530

    set

--- Objects/bagOfStrings.js
+++ Objects/bagOfStrings.js
@@ -1,27 +1,27 @@
 let str1 = "aabbcc";
 let str2 = "abd";
 
-// var obj = {};
-// for (let i = 0; i < str1.length; i++) {
-//   if (obj[str1[i]] == undefined) {
-//     obj[str1[i]] = 1;
-//   } else {
-//     obj[str1[i]]++;
-//   }
-// }
+var obj = {};
+for (let i = 0; i < str1.length; i++) {
+  if (obj[str1[i]] == undefined) {
+    obj[str1[i]] = 1;
+  } else {
+    obj[str1[i]]++;
+  }
+}
 
-// let str = "";
-// for (let key in obj) {
-//   str = str + key;
-//   console.log(key, obj[key]);
-//   console.log(str);
-// }
+let str = "";
+for (let key in obj) {
+  str = str + key;
+  console.log(key, obj[key]);
+  console.log(str);
+}
 
-// if (str == str2) {
-//   console.log("Yes");
-// } else {
-//   console.log("No");
-// }
+if (str == str2) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
 
 str1 = new Set(str1);
 str = [...str1].join("");
