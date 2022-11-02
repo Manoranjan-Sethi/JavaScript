0b44ad17536b92758a624b5d5ed3341c9c966b21
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-02 23:25:25 +0530

    imp

---
+++ Advance JS Syntax/arrowVsNormal.js
@@ -0,0 +1,52 @@
+//1. arguments objects
+
+//normal function
+function add(a, b) {
+  console.log(arguments);
+  return a + b;
+}
+
+add(2, 8);
+
+//arrow function
+
+const sum = (a, b) => {
+  console.log(arguments); // undefined
+  return a + b;
+};
+
+sum(2, 8);
+
+//2. this keyword
+
+//normal function
+var obj = {
+  name: "Ana",
+  print: function () {
+    console.log(`My name is ${this.name}`);
+  },
+};
+obj.print();
+
+//arrow function
+
+var obj = {
+  name: "Ana",
+  print: () => {
+    console.log(`My name is ${this.name}`); // undefined
+  },
+};
+obj.print();
+
+//3. implicit retun in arrow functions
+
+//normal function
+function get() {
+  700;
+}
+console.log(get()); // undefined
+
+//arrow function
+
+const get = () => 188;
+console.log(get());
