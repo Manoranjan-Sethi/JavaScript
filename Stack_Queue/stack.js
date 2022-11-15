599286b320c6431226f0cc539cda8b42c532af5e
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-15 20:18:12 +0530

    im

--- Stack_Queue/stack.js
+++ Stack_Queue/stack.js
@@ -3,33 +3,57 @@
 let arr = [];
 top = -1;
 
-function push(data){
+function push(data) {
   top++;
   arr[top] = data;
 }
 
-function pop(){
-
-  if(top == -1){
+function pop() {
+  if (top == -1) {
     return empty;
-  }else{
+  } else {
     ans = arr[top];
     top--;
     return ans;
   }
 }
 
-function peek(){
+function peek() {
   return arr[top];
 }
 
-
 // Inbuilt functions of stack
 
-arr = [1,2,3,4,5];
+arr = [1, 2, 3, 4, 5];
 arr.push(6);
 console.log(arr);
 arr.pop();
 arr.pop();
 arr.peek();
 console.log(arr);
+
+function isValid(str) {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "(") {
+      stack.push(str[i]);
+    } else {
+      if (stack.length == 0) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length == 0;
+}
+function generate(n, str, arr) {
+  if (n == 0) {
+    if (isValid(str)) {
+      arr.push(str);
+    }
+    return;
+  }
+
+  generate(n - 1, str + "(", arr);
+  generate(n - 1, str + ")", arr);
+}
